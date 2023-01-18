68b0f206395bbff46e530d23c98052b30f107dae
Aleksandr Demidenko <sashatro@mail.ru>
2023-01-18 21:41:08 +0300

    feat: Change topnav contacts

--- src/components/App/Main/Topnav/Topnav.js
+++ src/components/App/Main/Topnav/Topnav.js
@@ -27,27 +27,27 @@ function Topnav(props) {
         <ul className="contacts topnav__contacts">
           <li className="contacts__item">
             <h3 className="contacts__title">Режим работы:</h3>
-            <p className="contacts__hours">Пн-пт с 00:00 до 00:00</p>
+            <p className="contacts__hours">Круглосуточно</p>
           </li>
           <li className="contacts__item">
             <h3 className="contacts__title">Почта:</h3>
             <a href="mailto:testform@sitepro.by" className="contacts__email">
-              alvde@yandex.ru
+            masel.s@mail.ru
             </a>
           </li>
           <li className="contacts__item">
             <h3 className="contacts__title">Телефон:</h3>
             <a href="tel:+375000000000" className="contacts__tel">
-              +375(00) 000-00-00
+            +375(29) 241-55-67
             </a>
           </li>
         </ul>
         <div className="topnav__smallscreen-item">
           <a href="mailto:testform@sitepro.by" className="topnav__email">
-            alvde@yandex.ru
+          masel.s@mail.ru
           </a>
           <a href="tel:+375000000000" className="topnav__tel">
-            +375(00) 000-00-00
+          +375(29) 241-55-67
           </a>
         </div>
         <div className="topnav__links-wrapper">
